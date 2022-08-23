7a079687790f4a6301c197ac8844122ef5a8f9bb
Desta L <79503281+dlwub@users.noreply.github.com>
2022-08-23 10:45:22 +0300

    Update addTask.js

--- src/modules/addTask.js
+++ src/modules/addTask.js
@@ -3,7 +3,7 @@ import getLocalStorage from './getStorage.js';
 import Task from './Task.js';
 
 const addTask = (description) => {
-  // Add task to booksArray
+  // Add task to taskArray
   if (description) {
     let taskArray = getLocalStorage();
     const task = new Task(description, false, taskArray.length + 1);
