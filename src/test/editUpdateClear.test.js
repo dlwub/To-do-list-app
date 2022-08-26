8661427a2552957e8e7566f2556edf4ccf44ee04
Maaloum <50062179+maaloum@users.noreply.github.com>
2022-08-26 12:13:35 +0300

    Creation of ClearAll test function

--- src/test/editUpdateClear.test.js
+++ src/test/editUpdateClear.test.js
@@ -3,6 +3,8 @@ import setLocalStorage from '../modules/setStorage.js';
 import 'jest-localstorage-mock';
 import editTask from '../modules/editTask.js';
 import addTask from '../modules/addTask.js';
+import updateStatus from '../modules/updateStatus.js';
+import removeTask from '../modules/removeTask.js';
 import displayList from '../modules/displayList.js';
 
 describe('Edit task, update status, and clear completed', () => {
@@ -17,5 +19,16 @@ describe('Edit task, update status, and clear completed', () => {
     const editable = document.getElementById('label-1');
     expect(taskArray[0].description).toEqual(editable.textContent);
     setLocalStorage(taskArray);
-  });   
+  });
+  test('Clear all completed tasks', () => {
+    setLocalStorage([]); // Clear the local storage
+    addTask('one');
+    addTask('two');
+    addTask('three');
+    updateStatus(1, true);
+    updateStatus(3, true);
+    removeTask(-1);
+    const taskArray = getLocalStorage();
+    expect(taskArray.length).toBe(1);
+  });
 });
